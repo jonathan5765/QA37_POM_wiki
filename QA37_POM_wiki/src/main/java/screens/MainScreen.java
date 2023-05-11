9084c18e3717415de903797ce4fd1b67575aa8e2
jonathan5765 <jonathan5765@gmail.com>
2023-05-11 10:06:18 +0300

    start project

---
+++ QA37_POM_wiki/src/main/java/screens/MainScreen.java
@@ -0,0 +1,11 @@
+package screens;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
+public class MainScreen extends BaseScreen{
+
+    public MainScreen(AppiumDriver<MobileElement> driver) {
+        super(driver);
+    }
+}
---
+++ QA37_POM_wiki/src/test/java/tests/Launch.java
@@ -0,0 +1,10 @@
+package tests;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import manage.ConfigurationWiki;
+
+public class Launch extends ConfigurationWiki {
+
+
+}
